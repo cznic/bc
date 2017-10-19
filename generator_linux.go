f42b7addc91ca6bbc1be816a6a94c14f12de395f
Jan Mercl <0xjnml@gmail.com>
2017-10-19 23:12:27 +0200

    generator.go: Use FTP mirror.
    	modified:   generator_linux.go

--- generator_linux.go
+++ generator_linux.go
@@ -1,4 +1,4 @@
-//go:generate wget --no-clobber http://mirror.gutscheinrausch.de/gnu/bc/bc-1.07.tar.gz
+//go:generate wget --no-clobber http://ftpmirror.gnu.org/bc/bc-1.07.tar.gz
 //go:generate rm -rf bc-1.07/
 //go:generate tar xzf bc-1.07.tar.gz
 //go:generate sh -c "cp bc-1.07/AUTHORS bc-1.07/COPYING* bc-1.07/NEWS bc-1.07/README ."
